fcc638813f0da74953071c0042506b351823747d
TrueShinAkuma <shinakuma.work@gmail.com>
2020-09-20 06:00:36 +0500

    homework 10

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,19 @@
-$(function(){
-
-  $('.your-class').slick({
-    setting-name: setting-value
+$(function () {
+  $(".slider__inner").slick({
+    rows: 0,
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
+    appendArrows: '.slider-arrows',
   });
 
+  $(".rate-star").rateYo({
+    rating: 4.5,
+    starWidth: "13px",
+    readOnly: true,
+    ratedFill: "#ffc000"
+  });
+
+  var mixer = mixitup('.products__inner-box');
 });
